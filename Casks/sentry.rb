0539e4e08697a0b3ee745d64ea79ded208d44cbb
Monu Kumar <monu.kumar3@phonepe.com>
2026-08-10 21:47:34 +0530

    update: sentry: v2.0

--- Casks/sentry.rb
+++ Casks/sentry.rb
@@ -1,12 +1,11 @@
-
 cask "sentry" do
-  version "1.6"
-  sha256 "b3612614ec1df14b60c7e0d1cd6dacbf7150d331365e46cc7c8a5145e305c5cd"
+  version "2.0"
+  sha256 "8bd49020b51c8148c4391d62ee6221bfb5a7f0cb128bd4a38d83bbdd7d37c827"
 
   url "https://github.com/monuk7735/sentry/releases/download/v#{version}/Sentry-v#{version}.dmg",
       verified: "github.com/monuk7735/sentry/"
   name "Sentry"
-  desc "Secure your workflow without stopping it."
+  desc "Secure your workflow without stopping it"
   homepage "https://monuk7735.github.io/sentry/"
 
   livecheck do
@@ -14,7 +13,7 @@ cask "sentry" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "Sentry.app"
   binary "#{appdir}/Sentry.app/Contents/Resources/sentry-cli"
@@ -26,6 +25,6 @@ cask "sentry" do
   zap trash: [
     "~/Library/Application Support/Sentry",
     "~/Library/Preferences/com.monuk7735.sentry.plist",
-    "~/Library/Saved Application State/com.monuk7735.sentry.savedState"
+    "~/Library/Saved Application State/com.monuk7735.sentry.savedState",
   ]
 end
